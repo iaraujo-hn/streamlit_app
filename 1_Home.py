ad56333738a8ef80df542ca51bf466454ddc50d8
iaraujo-hn <iaraujo@horizon-next.com>
2024-07-29 15:33:59 -0400

    Update 1_Home.py

--- 1_Home.py
+++ 1_Home.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-
 # Page config
 st.set_page_config(page_title="Testing Tool", layout="wide")
 
@@ -8,11 +7,11 @@ st.set_page_config(page_title="Testing Tool", layout="wide")
 st.markdown("""
     <style>
     .link {
-        font-size: 22px;
+        font-size: 24px;
         text-decoration: none;
         color: black !important;
         font-weight: bold;
-        # background-color: rgba(43, 74, 153, 0.05);
+        background-color: rgba(43, 74, 153, 0.05);
         padding: 5px; 
         border-radius: 10px;
     }
@@ -32,14 +31,16 @@ st.markdown("##### Unlock the power of data-driven decisions with our collection
 
 st.markdown("--------------------")
 st.markdown("### Our Tools: ")
-
-st.markdown('<a href="pages/2_Significance Test.py" class="link">1. Significance Testing</a>', unsafe_allow_html=True)
+st.markdown("##### 1. Significance Testing: ")
+# st.markdown('<a href="pages/4_Significance Test.py" class="link"> Significance Test </a>', unsafe_allow_html=True)
 st.write('Perform a two-sample t-test to determine if your results are statistically significant. This tool helps assess whether the differences observed are meaningful.')
 
-st.markdown('<a href="pages/3_Sample Size Estimator.py" class="link">2. Sample Size Estimator</a>', unsafe_allow_html=True)
+st.markdown("##### 2. Sample Size Estimator: ")
+# st.markdown('<a href="pages/3_Sample Size Estimator.py" class="link"> Sample Size Estimator </a>', unsafe_allow_html=True)
 st.write('Maximize your chances of achieving statistical significance in your campaigns. This tool calculates the necessary sample size for control and test groups in an A/B test, aiding in budget planning and campaign duration.')
 
-st.markdown('<a href="pages/4_Campaign Duration Simulator.py" class="link">3. Campaign Duration Simulator</a>', unsafe_allow_html=True)
+st.markdown("##### 3. Campaign Duration Simulator: ")
+# st.markdown('<a href="pages/3_Sample Size Estimator.py" class="link"> Sample Size Estimator </a>', unsafe_allow_html=True)
 st.write('Estimate the optimal campaign duration when factors like lift and control group size are uncertain. This simulation tool helps you plan effectively by considering various scenarios.')
 
 # streamlit run 1_Home.py --server.enableXsrfProtection false 
